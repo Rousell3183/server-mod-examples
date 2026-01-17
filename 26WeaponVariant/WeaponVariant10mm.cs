41a4f8914454015cdd8d5caa919e59d65249656d
Ryan Rousell <rousell3183@saskpolytech.ca>
2026-01-17 13:39:26 -0600

    Refactor WeaponVariant10mm class and implement creation of 10mm weapon variants

--- 26WeaponVariant/WeaponVariant10mm.cs
+++ 26WeaponVariant/WeaponVariant10mm.cs
@@ -5,7 +5,7 @@ using SPTarkov.Server.Core.Models.Spt.Mod;
 using SPTarkov.Server.Core.Models.Utils;
 using SPTarkov.Server.Core.Services.Mod;
 
-namespace _18CustomItemService;
+namespace _26WeaponVariant;
 
 public record ModMetadata : AbstractModMetadata
 {
@@ -24,47 +24,130 @@ public record ModMetadata : AbstractModMetadata
     public override string License { get; init; } = "MIT";
 }
 
+// Unique IDs for our custom items
+public static class Custom10mmIds
+{
+    public const string AMMO_10MM_JHP = "696bbd26ddc277320902e641";
+    public const string MP5_10MM_RECEIVER = "696bbd26ddc277320902e63f";
+    public const string UMP45_10MM_RECEIVER = "696bbd26ddc277320902e642";
+    public const string CHAMBER_SLOT_ID = "696bbd26ddc277320902e643";
+}
+
 [Injectable(TypePriority = OnLoadOrder.PostDBModLoader + 1)]
-public class CustomItemServiceExample(
-    ISptLogger<CustomItemServiceExample> logger,
+public class WeaponVariant10mm(
+    ISptLogger<WeaponVariant10mm> logger,
     CustomItemService customItemService) : IOnLoad
 {
 
     public Task OnLoad()
     {
+        logger.Info("Starting 10mm weapon variant creation");
 
-        var ammo10mmTpls = new[]
-        {
-            ItemTpl.AMMO_10X25_AUTO_FMJ,
-            ItemTpl.AMMO_10X25_AUTO_JHP,
-            ItemTpl.AMMO_10X25_AUTO_AP,
-            ItemTpl.AMMO_10X25_AUTO_SAP,
-            ItemTpl.AMMO_10X25_AUTO_FTX,
-        };
+        // Step 1: Create 10mm JHP Ammo
+        CreateAmmo10mmJhp();
 
-        //Example of adding new item by cloning an existing item using `createCloneDetails`
-        var cloneMP5 = new NewItemFromCloneDetails
+        // Step 2: Create MP5 10mm Receiver
+        CreateMP510mmReceiver();
+
+        // Step 3: Create UMP45 10mm Receiver
+        CreateUMP4510mmReceiver();
+
+        logger.Success("10mm weapon variants created successfully");
+        return Task.CompletedTask;
+    }
+
+    private void CreateAmmo10mmJhp()
+    {
+        var ammo10mmJhp = new NewItemFromCloneDetails
         {
-            ItemTplToClone = ItemTpl.RECIEVER_MP5_9X19,
-            // ParentId refers to the Node item the gun will be under, you can check it in https://db.sp-tarkov.com/search
-            ParentId = "55818a304bdc2db5418b457d",
-            // The new id of our cloned item - MUST be a valid mongo id, search online for mongo id generators
-            NewId = "696bbd26ddc277320902e63f",
-            // Flea price of item
-            FleaPriceRoubles = 75000,
-            // Price of item in handbook
-            HandbookPriceRoubles = 55000,
-            // Handbook Parent Id refers to the category the gun will be under
-            HandbookParentId = "5b5f75b986f77447ec5d7710",
-            //you see those side box tab thing that only select gun under specific icon? Handbook parent can be found in Spt_Data\Server\database\templates.
+            ItemTplToClone = ItemTpl.AMMO_9X19_QUAKEMAKER, // Clone QuakeMaker as base (JHP profile)
+            ParentId = "5485a8684bdc2da71d8b4567", // Ammunition parent ID
+            NewId = Custom10mmIds.AMMO_10MM_JHP,
+            FleaPriceRoubles = 450,
+            HandbookPriceRoubles = 400,
+            HandbookParentId = "5b47574386f77428ca22b33b", // Ammo handbook category
             Locales = new Dictionary<string, LocaleDetails>
             {
                 {
                     "en", new LocaleDetails
                     {
-                        Name = "MP5 10mm Reciever",
-                        ShortName = "MP5 10mm Reciever",
-                        Description = "MP5 reciever chambered in 10mm Auto."
+                        Name = "10x25mm JHP",
+                        ShortName = "10mm JHP",
+                        Description = "10x25mm Auto JHP (Jacketed Hollow Point) cartridge with 180-grain hollow-point bullet. Designed for maximum stopping power with excellent expansion characteristics. Offers superior ballistic performance compared to 9mm with higher velocity and energy."
+                    }
+                }
+            },
+            OverrideProperties = new TemplateItemProperties
+            {
+                // Ballistics - scaled from 9x19 Quakemaker to 10mm
+                Damage = 135, // 85 * 1.58x multiplier for 10mm power
+                PenetrationPower = 11, // Slightly higher due to caliber, but JHP reduces penetration
+                ArmorDamage = 32, // Scaled armor damage
+                InitialSpeed = 420, // Higher velocity for 10mm
+                
+                // Physical properties
+                BulletDiameterMilimeters = 10,
+                BulletMassGram = 11.66f, // 180 grains in grams
+                Caliber = "Caliber10mm",
+                Weight = 0.016f, // Heavier than 9mm
+                
+                // Ballistic coefficient and behavior
+                BallisticCoeficient = 0.140f,
+                SpeedRetardation = 0.0001f,
+                
+                // Bleeding and trauma
+                HeavyBleedingDelta = 0.25f, // Good expansion for JHP
+                LightBleedingDelta = 0.18f,
+                
+                // Fragmentation (JHP characteristic - expansion not fragmentation)
+                FragmentationChance = 0.08f, // Lower than armor-piercing
+                RicochetChance = 0.08f,
+                
+                // Durability and reliability
+                DurabilityBurnModificator = 1.12f,
+                MalfFeedChance = 0.110f,
+                MalfMisfireChance = 0.135f,
+                MisfireChance = 0.03f,
+                
+                // Heat and recoil
+                HeatFactor = 0.99f,
+                
+                // Ammo properties
+                AmmoTooltipClass = "Expansive",
+                RarityPvE = "Rare",
+                
+                // Stamina
+                StaminaBurnPerDamage = 0.192f,
+                
+                // Stack settings
+                StackMaxSize = 50,
+                StackMaxRandom = 40,
+                StackMinRandom = 15,
+            }
+        };
+
+        customItemService.CreateItemFromClone(ammo10mmJhp);
+        logger.Info("Created 10mm JHP ammunition");
+    }
+
+    private void CreateMP510mmReceiver()
+    {
+        var mp510mm = new NewItemFromCloneDetails
+        {
+            ItemTplToClone = ItemTpl.RECIEVER_MP5_9X19,
+            ParentId = "55818a304bdc2db5418b457d", // Assault rifle parent
+            NewId = Custom10mmIds.MP5_10MM_RECEIVER,
+            FleaPriceRoubles = 85000,
+            HandbookPriceRoubles = 65000,
+            HandbookParentId = "5b5f796a86f774093f2ed3c0", // Submachine gun handbook category
+            Locales = new Dictionary<string, LocaleDetails>
+            {
+                {
+                    "en", new LocaleDetails
+                    {
+                        Name = "MP5 10mm Receiver",
+                        ShortName = "MP5 10mm RX",
+                        Description = "MP5 upper receiver converted to fire 10x25mm ammunition. Maintains the renowned reliability and accuracy of the MP5 platform while stepping up to 10mm Auto power."
                     }
                 }
             },
@@ -75,8 +158,8 @@ public class CustomItemServiceExample(
                     new Slot
                     {
                         Name = "patron_in_weapon_000",
-                        Id = "61f7c9e189e6fb1a5e3ea791",
-                        Parent = "696bbd26ddc277320902e63f",
+                        Id = Custom10mmIds.CHAMBER_SLOT_ID,
+                        Parent = Custom10mmIds.MP5_10MM_RECEIVER,
                         Properties = new SlotProperties
                         {
                             Filters =
@@ -85,7 +168,7 @@ public class CustomItemServiceExample(
                                 {
                                     Filter =
                                     [
-                                        // TBD: Add 10mm Auto ammo TPLs here
+                                        Custom10mmIds.AMMO_10MM_JHP // Only accepts our custom 10mm JHP
                                     ]
                                 }
                             ]
@@ -98,8 +181,62 @@ public class CustomItemServiceExample(
             },
         };
 
-        customItemService.CreateItemFromClone(cloneMP5); // Send our data to the function that creates our item
-        
-        return Task.CompletedTask;
+        customItemService.CreateItemFromClone(mp510mm);
+        logger.Info("Created MP5 10mm receiver");
+    }
+
+    private void CreateUMP4510mmReceiver()
+    {
+        var ump4510mm = new NewItemFromCloneDetails
+        {
+            ItemTplToClone = ItemTpl.RECIEVER_UMP45,
+            ParentId = "55818a304bdc2db5418b457d", // Assault rifle parent
+            NewId = Custom10mmIds.UMP45_10MM_RECEIVER,
+            FleaPriceRoubles = 90000,
+            HandbookPriceRoubles = 70000,
+            HandbookParentId = "5b5f796a86f774093f2ed3c0", // Submachine gun handbook category
+            Locales = new Dictionary<string, LocaleDetails>
+            {
+                {
+                    "en", new LocaleDetails
+                    {
+                        Name = "UMP45 10mm Receiver",
+                        ShortName = "UMP45 10mm RX",
+                        Description = "UMP45 upper receiver chambered in 10x25mm Auto. Combines the versatile ergonomics and modularity of the UMP platform with the stopping power of 10mm ammunition."
+                    }
+                }
+            },
+            OverrideProperties = new TemplateItemProperties
+            {
+                Chambers =
+                [
+                    new Slot
+                    {
+                        Name = "patron_in_weapon_000",
+                        Id = "696bbd26ddc277320902e644", // Unique ID for UMP chamber
+                        Parent = Custom10mmIds.UMP45_10MM_RECEIVER,
+                        Properties = new SlotProperties
+                        {
+                            Filters =
+                            [
+                                new SlotFilter
+                                {
+                                    Filter =
+                                    [
+                                        Custom10mmIds.AMMO_10MM_JHP // Only accepts our custom 10mm JHP
+                                    ]
+                                }
+                            ]
+                        },
+                        Required = false,
+                        MergeSlotWithChildren = false,
+                        Prototype = "55d4af244bdc2d962f8b4571"
+                    }
+                ]
+            },
+        };
+
+        customItemService.CreateItemFromClone(ump4510mm);
+        logger.Info("Created UMP45 10mm receiver");
     }
 }
